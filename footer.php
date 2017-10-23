f675b8c123330dde41d595027effeef3a81c471f
Richard Bång <richard.bang@live.se>
2017-10-23 14:53:40 +0200

    footer link to event

--- footer.php
+++ footer.php
@@ -62,6 +62,10 @@
                 </a>
             </div>
         </div>
+        <div class="event-footer-link">
+            <p>Come visit us at <span>RSNA</span></p>
+            <a href="<?php echo home_url('/rsna-chicago-2017')?>">Read more</a>
+        </div>
     </section>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDXWF-zTaNyT5bR64le1M2Xrp7P6_8Shtw&callback=initMap" type="text/javascript"></script>
 </body>
